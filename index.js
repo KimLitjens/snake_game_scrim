c2d1adb93af7fea7d0395951048e29181b3f4565
Kim Litjens <Kimlitjens4@gmail.com>
2021-06-04 15:54:57 +0200

    add apple

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const score = document.getElementById('score')
 let squares = []
 let currentSnake = [2, 1, 0]
 let direction = 1
-let width = 10
+const width = 10
+let appleIndex = 0
 
 function createGrid() {
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < width * width; i++) {
         const square = document.createElement('div')
         square.classList.add('square')
         grid.appendChild(square)
@@ -20,6 +21,15 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+    if (
+        (currentSnake[0] + width >= width * width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width - 1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
+        squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+        return clearInterval(timerId)
+
     const tail = currentSnake.pop()
     squares[tail].classList.remove('snake')
     currentSnake.unshift(currentSnake[0] + direction)
@@ -28,11 +38,19 @@ function move() {
 
 let timerId = setInterval(move, 1000)
 
+function generateApples() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+}
+generateApples()
+
 function control(e) {
-    (e.keyCode === 39) ? console.log('right pressed')(direction = 1)
-        : (e.keyCode === 38) ? console.log('up pressed')(direction = -width)
-            : (e.keyCode === 37) ? console.log('left pressed')(direction = -1)
-                : (e.keyCode === 40) ? console.log('down pressed')(direction = +width)
+    (e.keyCode === 39) ? (direction = 1)
+        : (e.keyCode === 38) ? (direction = -width)
+            : (e.keyCode === 37) ? (direction = -1)
+                : (e.keyCode === 40) ? (direction = +width)
                     : ''
 }
 
